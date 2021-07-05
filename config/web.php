2d515cec4ec9e58ceeb89c85ef837e39e09b76e3
Ruslan.Androsenko <ruslan.androsenko@leomax.ru>
2021-07-05 17:43:11 +0300

    Created a module to implement REST API v1 requests ver. 1.2

--- config/web.php
+++ config/web.php
@@ -11,6 +11,11 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'v1' => [
+            'class' => 'app\modules\v1\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -57,6 +62,8 @@ $config = [
                 'collapseSlashes' => true,
             ],
             'rules' => [
+                '<module:\w+>/<controller:\w+>/<action:\w+>'=>'<module>/<controller>/<action>',
+                '<module:\w+><controller:\w+>/<action:update|delete>/<id:\d+>' => '<module>/<controller>/<action>',
             ],
         ],
     ],
